cdf08ed11313554004f54a4becb6821da127ba6b
Matko <matkomatic59@gmail.com>
2023-12-27 00:32:05 +0100

    Added jest config

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@self/(.*)$': '<rootDir>/src/$1',
+    '^@models/(.*)$': '<rootDir>/src/models/$1',
+  },
+ setupFiles: ['<rootDir>/tests/setup.ts'],
+ testMatch: ["<rootDir>/tests/**/*.test.ts"]
+};
